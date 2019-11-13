36ae29a4225d06643caf03a42b6cfe259e536b84
Vincent Rischmann <vincent@rischmann.fr>
2019-11-13 01:49:59 +0100

    promhistogram: implement analyzer

--- promhistogram/lib.go
+++ promhistogram/lib.go
@@ -5,15 +5,87 @@
 // code it's easy to forget.
 package promhistogram
 
-import "golang.org/x/tools/go/analysis"
+import (
+	"go/ast"
 
-var Analyzer = analysis.Analyzer{
-	Name: "promhistogram",
-	Doc:  "check that the Buckets field is set when creating a Prometheus histogram",
-	Run:  run,
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name:     "promhistogram",
+	Doc:      "check that the Buckets field is set when creating a Prometheus histogram",
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
+	Run:      run,
 }
 
-func run(pass *analysis.Pass) (interface{}, error) {
+var (
+// promPackageIdent = &ast.Ident{Name: "prometheus"}
+)
 
-	panic("not implemented")
+func run(pass *analysis.Pass) (interface{}, error) {
+	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	types := []ast.Node{
+		(*ast.CompositeLit)(nil),
+	}
+
+	inspect.Preorder(types, func(n ast.Node) {
+		lit := n.(*ast.CompositeLit)
+
+		// check if our literal is exactly prometheus.HistogramOpts
+
+		sel, ok := lit.Type.(*ast.SelectorExpr)
+		if !ok {
+			return
+		}
+		exprIdent, ok := sel.X.(*ast.Ident)
+		if !ok {
+			return
+		}
+
+		if exprIdent.Name != "prometheus" || sel.Sel.Name != "HistogramOpts" {
+			return
+		}
+
+		// it is, now check the different composite elements
+
+		// Prometheus requires only the Name field according to the documentation:
+		// https://godoc.org/github.com/prometheus/client_golang/prometheus#HistogramOpts
+		//
+		// In our case we also assume that the Buckets field must be set.
+
+		var (
+			hasName    bool
+			hasBuckets bool
+		)
+
+		for _, el := range lit.Elts {
+			kve := el.(*ast.KeyValueExpr)
+			if identIs(kve.Key, "Name") {
+				hasName = true
+			}
+			if identIs(kve.Key, "Buckets") {
+				hasBuckets = true
+			}
+		}
+
+		if !hasName {
+			pass.ReportRangef(lit, "literal HistogramOpts doesn't have the Name field defined")
+		}
+		if !hasBuckets {
+			pass.ReportRangef(lit, "literal HistogramOpts doesn't have the Buckets field defined")
+		}
+	})
+
+	return nil, nil
+}
+
+func identIs(ident ast.Expr, name string) bool {
+	v, ok := ident.(*ast.Ident)
+	if !ok {
+		return false
+	}
+	return v.Name == name
 }
